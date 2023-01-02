fd82bf3e3713b3d76565a6489610bc4417643d35
barisck94 <110248398+barisck94@users.noreply.github.com>
2023-01-03 01:34:44 +0300

    Update opustotext.py

--- opustotext.py
+++ opustotext.py
@@ -8,15 +8,15 @@ except:
     f.write("\n")
 wav_path = opus_path+".wav"
 opus_path = opus_path+".opus"
-os.system(f'ffmpeg -i "{opus_path}" -vn "convertttttttttttttttttttttttttttttttttttttting.wav"')
+os.system(f'ffmpeg -i "{opus_path}" -vn "converting.wav"')
 r = sr.Recognizer()
-with sr.AudioFile("convertttttttttttttttttttttttttttttttttttttting.wav") as source:
+with sr.AudioFile("converting.wav") as source:
     audio = r.record(source)
 try:
     s = r.recognize_google(audio, language="tr-tr")
     print("Text: "+s)
 except Exception as e:
     print("Exception: "+str(e))
-os.remove("convertttttttttttttttttttttttttttttttttttttting.wav")
+os.remove("converting.wav")
 f.write(s)
 f.close()
